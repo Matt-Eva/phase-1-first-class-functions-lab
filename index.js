770c869e53d10bccdbc5af78ae965d6af21f702f
Matt Eva <matteweva@gmail.com>
2021-10-13 10:04:20 -0700

    commit tree fcbdd0ff58fce06927f75a5c714c0d0b29720e65

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+
+returnFirstTwoDrivers(['Sally', 'Bob']);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(drivers.length - 2); 
+
+returnLastTwoDrivers(['Julia','Maggie','Greg','Peaches']);
+
+/*function selectingDrivers(drivers) {
+    [
+    returnFirstTwoDrivers(drivers),
+    returnLastTwoDrivers(drivers),
+    ]
+}*/
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+selectingDrivers[0](['Blebbie',  'Flebbie', 'Clebbie']);
+
+function createFareMultiplier(integer) {
+    return function(fare) {
+        return fare * integer;
+    }
+}
+
+createFareMultiplier(4)(4);
+
+const fareDoubler = function(fare){
+    return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = fare => createFareMultiplier(3)(fare);
+
+function selectDifferentDrivers(drivers, selectingDrivers){
+    return selectingDrivers(drivers);
+}
